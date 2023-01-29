26d75c85d581d1b33511997f1dc3bde87a929eca
Ankit Kumar Nayak <ankitnayak1726@gmail.com>
2023-01-29 08:24:22 +0530

    hashed route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { MoralisProvider } from "react-moralis";
 
 
@@ -11,9 +11,12 @@ import { MoralisProvider } from "react-moralis";
 ReactDOM.render(
   <React.StrictMode>
     <MoralisProvider appId="xxxx" serverUrl="xxxx">
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter>
+
         <App />
-      </BrowserRouter>
+      </HashRouter>
+      {/* </BrowserRouter> */}
     </MoralisProvider>
   </React.StrictMode>,
   document.getElementById('root')
